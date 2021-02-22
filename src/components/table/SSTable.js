11afe160702e343934af7c32925e8206d56da361
Matheus Wendel <matheus_wendel@outlook.com>
2021-02-21 23:53:34 -0300

    Componetne de tabela

---
+++ src/components/table/SSTable.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory, {
+  PaginationListStandalone,
+  PaginationProvider,
+} from "react-bootstrap-table2-paginator";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
+export default class SSTable extends Component {
+  render() {
+    const paginationOption = {
+      custom: true,
+      sizePerPage: 10,
+      hideSizePerPage: true,
+      hidePageListOnlyOnePage: true,
+      alwaysShowAllBtns: true,
+      totalSize: this.props.data.length,
+    };
+    const SStable = ({ paginationProps, paginationTableProps }) => (
+      <div>
+        <BootstrapTable
+          wrapperClasses="table-responsive"
+          hover
+          bootstrap4={true}
+          noDataIndication="Sem resultados."
+          keyField="id"
+          data={this.props?.data || []}
+          columns={this.props?.columns}
+          {...paginationTableProps}
+        />
+        <div className="col d-flex justify-content-center">
+          <PaginationListStandalone {...paginationProps} />
+        </div>
+      </div>
+    );
+    return (
+      <div>
+        <PaginationProvider pagination={paginationFactory(paginationOption)}>
+          {SStable}
+        </PaginationProvider>
+      </div>
+    );
+  }
+}

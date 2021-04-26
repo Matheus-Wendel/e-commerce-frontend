2af853d44da1a5c0e9a8c3e3c29b6326f1b1fc93
Matheus Wendel <matheus_wendel@outlook.com>
2021-04-26 15:45:47 -0300

    refatorado tela minhas compras  do cliente

--- src/views/client-purchases/client-purchases.js
+++ src/views/client-purchases/client-purchases.js
@@ -4,34 +4,54 @@ import clone from "clone";
 import { Component } from "react";
 import { Card } from "react-bootstrap";
 import SSAlert from "../../components/alert/SSalert";
+import SSForm from "../../components/form/SSForm";
+import PurchaseItemDevolutionForm from "../../components/purchases/PurchaseItemDevolutionForm";
+import PurchaseItemsTable from "../../components/purchases/PurchaseItemsTable";
 import PurchaseTable from "../../components/purchases/PurchaseTable";
 import SSFormLayout from "../../layout/SSFormLayout";
-import { apiDelete, apiGet, apiPost, apiPut } from "../../utils/api/api-utils";
-import { alertMessageUtil, updateStateValue } from "../../utils/utils";
+import { apiGet } from "../../utils/api/api-utils";
+import {
+  alertMessageUtil,
+  handleErrorMessage,
+  handleSetAlert,
+  updateStateValue,
+} from "../../utils/utils";
 import CreditCardModel from "./CreditCardModel";
-import PurchaseItemsTable from "../../components/purchases/PurchaseItemsTable";
-import PurchaseItemDevolutionForm from "../../components/purchases/PurchaseItemDevolutionForm";
-import SSForm from "../../components/form/SSForm";
-
+const querystring = require("querystring");
 export default class ClientPurchases extends Component {
   constructor(props) {
     super(props);
     this.state = {
       alert: alertMessageUtil(),
       purchaseItems: [],
+      purchases: [],
     };
   }
   async componentDidMount() {
-    // try {
-    //   await this.fetchCreditCards();
-    // } catch (error) {
-    //   this.handleSetMessages(
-    //     ["Não foi possivel carregar os seus cartões"],
-    //     true
-    //   );
-    // }
+    await this.fetchPurchases();
+    let status = querystring.parse(this.props.location.search);
+
+    if (status["?compra"] === "finalizada") {
+      handleSetAlert(
+        this.setState.bind(this),
+        ["Compra realizada com sucesso"],
+        "Sucesso",
+        "success"
+      );
+    }
   }
 
+  async fetchPurchases() {
+    try {
+      let purchases = await apiGet(process.env.REACT_APP_PURCHASE_ENDPOINT);
+
+      this.setState({
+        purchases,
+      });
+    } catch (error) {
+      handleErrorMessage(this.setState.bind(this), error);
+    }
+  }
   async handlePreventDefaut(event) {
     event.preventDefault();
     event.stopPropagation();
@@ -43,12 +63,6 @@ export default class ClientPurchases extends Component {
     this.setState({
       purchaseItems: selectedRow.purchaseItems,
     });
-  }
-
-  handleSetMessages(messages = [], show = false, title = "", variant = "") {
-    this.setState(() => ({
-      alert: alertMessageUtil(messages, show, title, variant),
-    }));
   }
 
   handleClear() {
@@ -82,7 +96,10 @@ export default class ClientPurchases extends Component {
           />
           <hr />
           <h3>Suas Compras</h3>
-          <PurchaseTable onRowSelect={this.handleSelectedRow.bind(this)} />
+          <PurchaseTable
+            data={this.state.purchases}
+            onRowSelect={this.handleSelectedRow.bind(this)}
+          />
           <h3>Itens da compra</h3>
           <PurchaseItemsTable
             data={this.state.purchaseItems}

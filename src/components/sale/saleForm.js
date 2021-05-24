5e85c7dd005eae4c4cd4803e08cc7278ffaef679
Matheus Wendel <matheus_wendel@outlook.com>
2021-05-23 23:06:39 -0300

    cadastro de promocoes

--- src/components/sale/saleForm.js
+++ src/components/sale/saleForm.js
@@ -3,6 +3,7 @@ import { Col, Form } from "react-bootstrap";
 import SSInput from "../form/SSInput";
 import SSSelect from "../form/SSSelect";
 import { apiGet } from "../../utils/api/api-utils";
+import { formatPercentageValue } from "../../utils/utils";
 
 export default class SaleForm extends Component {
   render() {
@@ -13,27 +14,32 @@ export default class SaleForm extends Component {
             <SSInput
               label="Margem de lucro promocional"
               type="range"
-              min="1"
-              max="500"
-              step="1"
+              min="0.01"
+              max="5"
+              step="0.01"
               name={`${this.props.root}.profit`}
               value={this.props?.sale?.profit || ""}
               onChange={this.props.onChange}
+              disabled={this.props.disabled}
             />
-            <SSInput value={this.props?.sale?.profit || ""} disabled />
+            <SSInput
+              value={formatPercentageValue(this.props?.sale?.profit || 0) || ""}
+              disabled
+            />
           </Form.Group>
         </Form.Row>
         <Form.Row>
           <Form.Group as={Col} md={5}>
             <SSSelect
               label="Status"
-              name={`${this.props.root}.genre`}
+              name={`${this.props.root}.status`}
               items={[
                 { id: "ACTIVE", description: "Ativa" },
                 { id: "INACTIVE", description: "Inativa" },
               ]}
-              value={this.props?.client?.genre || ""}
+              value={this.props?.sale?.status || ""}
               onChange={this.props.onChange}
+              disabled={this.props.disabled}
             />
           </Form.Group>
         </Form.Row>
--- src/views/sale/Sale.js
+++ src/views/sale/Sale.js
@@ -4,154 +4,90 @@ import clone from "clone";
 import { Component } from "react";
 import { Card } from "react-bootstrap";
 import SSAlert from "../../components/alert/SSalert";
-import DiscTable from "../../components/disc/discTable";
 import SSForm from "../../components/form/SSForm";
 import PricingTable from "../../components/pricing/pricingTable";
 import SaleForm from "../../components/sale/saleForm";
 import SSFormLayout from "../../layout/SSFormLayout";
-import { apiDelete, apiPost, apiPut } from "../../utils/api/api-utils";
-import { alertMessageUtil, updateStateValue } from "../../utils/utils";
+import { apiDelete, apiGet, apiPost, apiPut } from "../../utils/api/api-utils";
+import {
+  alertMessageUtil,
+  handleErrorMessage,
+  handleSetAlert,
+  updateStateValue,
+} from "../../utils/utils";
 import SaleModel from "./SaleModel";
+import PricingModel from "../pricing/PricingModel";
 
-export default class Sale extends Component {
+export default class Pricing extends Component {
   constructor(props) {
     super(props);
     this.state = {
       alert: alertMessageUtil(),
-      sale: new SaleModel(),
+      pricing: new PricingModel(),
       isUpdate: false,
-      adresses: [],
+      prices: [],
     };
   }
   async componentDidMount() {
-    // try {
-    //   await this.fetchAdresses();
-    // } catch (error) {
-    //   this.handleSetMessages(
-    //     ["Não foi possivel carregar os seus endereços"],
-    //     true
-    //   );
-    // }
+    await this.fetchPrices();
+  }
+  async fetchPrices() {
+    try {
+      let prices = await apiGet(process.env.REACT_APP_PRICING_ENDPOINT);
+      console.log(prices);
+      this.setState({
+        prices,
+      });
+    } catch (error) {
+      handleErrorMessage(this.setState.bind(this), error);
+    }
   }
 
-  async handlePreventDefaut(event) {
+  async handleSubmit(event) {
     event.preventDefault();
     event.stopPropagation();
+
+    try {
+      const { pricing, isUpdate } = this.state;
+      if (isUpdate) {
+        await apiPut(process.env.REACT_APP_SALE_ENDPOINT, pricing.sale);
+        handleSetAlert(
+          this.setState.bind(this),
+          ["Promoção atualizada com sucesso"],
+          "sucesso",
+          "success"
+        );
+      } else {
+        await apiPost(process.env.REACT_APP_SALE_ENDPOINT, pricing.sale);
+        handleSetAlert(
+          this.setState.bind(this),
+          ["Promoção cadastrada com sucesso"],
+          "sucesso",
+          "success"
+        );
+      }
+      this.handleClear();
+      await this.fetchPrices();
+    } catch (error) {
+      handleErrorMessage(this.setState.bind(this), error);
+    }
   }
 
   handleSelectedRow(row) {
     let selectedRow = clone(row);
+
+    console.log(selectedRow);
     this.setState({
-      sale: selectedRow,
       isUpdate: true,
+      pricing: selectedRow,
     });
-  }
-  handleSetMessages(messages = [], show = false, title = "", variant = "") {
-    this.setState(() => ({
-      alert: alertMessageUtil(messages, show, title, variant),
-    }));
   }
   handleClear() {
     this.setState({
-      sale: new SaleModel(),
       isUpdate: false,
+      pricing: new PricingModel(),
     });
   }
-
-  async handleDelete() {
-    if (!this.state.isUpdate) {
-      return;
-    }
-
-    const deliveryAddress = clone(this.state.deliveryAddress);
-
-    try {
-      await apiDelete(
-        process.env.REACT_APP_ADDRESS_ENDPOINT,
-        deliveryAddress.id
-      );
-
-      this.handleClear();
-      this.handleSetMessages(
-        [
-          `Endereço de entrega ${deliveryAddress.description} excluido com sucesso`,
-        ],
-        true,
-        "Sucesso",
-        "success"
-      );
-      await this.fetchAdresses();
-    } catch (error) {
-      if (error.response?.data?.error) {
-        this.handleSetMessages(error.response?.data?.error.split(";;"), true);
-        return;
-      }
-      if (error.response?.data?.message) {
-        this.handleSetMessages(
-          [`${error.response?.data.error} : ${error.response?.data.message}`],
-          true
-        );
-
-        return;
-      }
-    }
-  }
-  async handleSubimit(event) {
-    event.preventDefault();
-    if (event.currentTarget.checkValidity() === false) {
-      event.stopPropagation();
-      this.setState(() => ({
-        formError: "true",
-      }));
-      return;
-    }
-    this.setState(() => ({
-      formError: "false",
-    }));
-
-    const deliveryAddress = clone(this.state.deliveryAddress);
-
-    try {
-      if (this.state.isUpdate) {
-        await apiPut(process.env.REACT_APP_ADDRESS_ENDPOINT, deliveryAddress);
-        this.handleSetMessages(
-          [
-            `Endereço de entrega ${deliveryAddress.description} atualizado com sucesso`,
-          ],
-          true,
-          "Sucesso",
-          "success"
-        );
-      } else {
-        await apiPost(process.env.REACT_APP_ADDRESS_ENDPOINT, deliveryAddress);
-        this.handleSetMessages(
-          [
-            `Endereço de entrega ${deliveryAddress.description} salvo com sucesso`,
-          ],
-          true,
-          "Sucesso",
-          "success"
-        );
-      }
-      this.handleClear();
-      await this.fetchAdresses();
-    } catch (error) {
-      if (error.response?.data?.error) {
-        this.handleSetMessages(error.response?.data?.error.split(";;"), true);
-
-        return;
-      }
-      if (error.response?.data?.message) {
-        this.handleSetMessages(
-          `${error.response?.data.error} : ${error.response?.data.message}`,
-          true
-        );
-
-        return;
-      }
-    }
-  }
-
   async handleInputChange(event) {
     const target = event.target;
     let { name, value } = target;
@@ -166,7 +102,7 @@ export default class Sale extends Component {
         <Card.Body>
           <Card.Title as="h1">
             <FontAwesomeIcon icon={faCompactDisc} spin className="mr-2" />
-            Controle de promoções
+            Cadastro de Promoções
           </Card.Title>
           <SSAlert
             showAlert={this.state.alert.show}
@@ -177,22 +113,25 @@ export default class Sale extends Component {
           <hr />
 
           <SSForm
-            onSubmit={this.handleSubimit.bind(this)}
+            onSubmit={this.handleSubmit.bind(this)}
             onCancel={this.handleClear.bind(this)}
-            onDelete={this.handleDelete.bind(this)}
-            allowDelete={this.state.isUpdate}
-            customSubmitText={this.state.isUpdate ? "Atualizar" : ""}
+            customSubmitText={"Atualizar"}
+            disabled={!this.state.isUpdate}
           >
             <SaleForm
-              root="sale"
-              sale={this.state.sale}
+              root="pricing.sale"
+              sale={this.state.pricing?.sale}
               onChange={this.handleInputChange.bind(this)}
+              disabled={!this.state.isUpdate}
             />
             <hr />
           </SSForm>
           <hr />
-          <h3>Precificações</h3>
-          <PricingTable onRowSelect={this.handleSelectedRow.bind(this)} />
+          <h3>Precificações cadastradas</h3>
+          <PricingTable
+            onRowSelect={this.handleSelectedRow.bind(this)}
+            data={this.state.prices}
+          />
         </Card.Body>
       </SSFormLayout>
     );
